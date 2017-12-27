6ebeb395603b0919ada9118bab81076b6cc601f5
dark777 <34851515+dark777@users.noreply.github.com>
2017-12-27 19:37:58 -0200

    Sources code C/C++

--- ProgrammingModules/hello.c
+++ ProgrammingModules/hello.c
@@ -8,8 +8,7 @@ static int __init lkp_init(void)
      printk("<1> Hello, World! from the kernel space...\n");
      return 0;
 }
- 
- 
+
 static void __exit lkp_cleanup(void)
 {
      printk("<1> Goodbye, World! Leaving kernel space...\n");
