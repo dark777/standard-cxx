a8263e07fbcb23b82087b31826675537bc7443f1
Jean Zonta <wiki.anon@yahoo.com.br>
2018-01-19 03:12:24 -0200

    Sources code C/C++

--- KernelModules/hello.c
+++ KernelModules/hello.c
@@ -4,14 +4,13 @@
  
 static int __init lkp_init(void)
 {
- 
-     printk("<1> Hello, World! from the kernel space...\n");
-     return 0;
+ printk("<1> Hello, World! from the kernel space...\n");
+ return 0;
 }
 
 static void __exit lkp_cleanup(void)
 {
-     printk("<1> Goodbye, World! Leaving kernel space...\n");
+ printk("<1> Goodbye, World! Leaving kernel space...\n");
 }
  
  module_init(lkp_init);
