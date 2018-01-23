4cecea46f304b7500591c0911e3bcfa6ddbcadab
Jean Zonta <wiki.anon@yahoo.com.br>
2018-01-23 02:44:53 -0200

    Sources code C/C++

--- libmenu/char/main.cxx
+++ libmenu/char/main.cxx
@@ -37,7 +37,7 @@ int main()
  int op;
  int tam=4;
  const char* opcoes[tam]={"Novo","Consultar","Listar","Sair"};
- //menu(tam, opcoes); // aceita até 15 opçoes
+ //menu(tam, opcoes); // aceita limite maximo de até 15 opçoes
  menu(tam, tam, opcoes); // aceita limite igual ao tamanho do vetor
  std::cin>>op;
  return 0;
--- libmenu/string/main.cxx
+++ libmenu/string/main.cxx
@@ -38,7 +38,7 @@ int main()
  int op;
  int tam=4;
  std::string opcoes[tam]={"Novo","Consultar","Listar","Sair"};
- //menu(tam, opcoes); // aceita até 15 opçoes
+ //menu(tam, opcoes); // aceita limite maximo de até 15 opçoes
  menu(tam, tam, opcoes); // aceita limite igual ao tamanho do vetor
  std::cin>>op;
  return 0;
