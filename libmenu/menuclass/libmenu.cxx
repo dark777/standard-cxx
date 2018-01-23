1de8b57bbf2984a0b61a658ee05d08816c82445f
Jean Zonta <wiki.anon@yahoo.com.br>
2018-01-23 02:53:10 -0200

    Sources code C/C++

--- libmenu/menuclass/libmenu.cxx
+++ libmenu/menuclass/libmenu.cxx
@@ -1,7 +1,28 @@
 #include "libmenu.hxx"
 
 #define MAX 15
-
+/*!
+ *
+ * @begin @section terms_of_use Terms of Use
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * @end @section terms_of_use Terms of Use
+ * 
+ * @begin @section author Author
+ * 
+ * @file       libmenu.cxx
+ * @version    0.1
+ * @brief      menu para const char* e std::string*
+ * @author     Jean Zonta
+ * @Copyright (C) 2013-2017 Jean Zonta.
+ * 
+ * @end @section author Author
+ *
+*/
 menu::menu(int opmenu, const char* opcoes[])
 {
  if(opmenu > MAX)
