c6ffabd802d8f68f0d9899b70e97d106267740ea
Jean Zonta <wiki.anon@yahoo.com.br>
2018-01-25 00:36:35 -0200

    Sources code C/C++

--- Composition/date/ex1/date.cxx
+++ Composition/date/ex1/date.cxx
@@ -27,7 +27,7 @@ struct Date
     if(syear.length() == 1)
     syear.insert(0,"0");
     
-    day = std::to_string(ltm->tm_mday);   
+    day = std::to_string(ltm->tm_mday);
     month = std::to_string(ltm->tm_mon+1);
     year = std::to_string(ltm->tm_year+1900);
    }
@@ -40,32 +40,46 @@ struct Date
      
      if(day.length() != 0)day.clear();
      if(month.length() != 0)month.clear();
-     if(year.length() != 0)year.clear();     
+     if(year.length() != 0)year.clear();
     }
    
    std::string toInt()
    {
-    return std::string(day+"/"+month+"/"+year);  
+    return std::string(day+"/"+month+"/"+year);
    }
    
    std::string toString()
    {
-    return std::string(sday+"/"+smonth+"/"+syear);  
+    return std::string(sday+"/"+smonth+"/"+syear);
    }
+   
+   std::string dateTime()
+   {
+    strftime(buf, sizeof(buf), "%a, %d %b %Y %H:%M:%S GMT", now);
+    return buf;
+   }
+   
+  private:
+  
+  char buf[50];
+  time_t tms = time(NULL);
+  tm *now = localtime(&tms);
+  
 }date; 
 
 int main()
 {
  std::cout << "\n\tThe date is Int "
-           << "\n\tThe day is "   << date.day      
-           << "\n\tThe month is " << date.month
-           << "\n\tThe year is "  << date.year
-           << "\n\tThe date Int Format "  << date.toInt()
+           << "\n\tThe day is.............: " << date.day      
+           << "\n\tThe month is...........: " << date.month
+           << "\n\tThe year is............: " << date.year
+           << "\n\tThe date Int Format....: " << date.toInt()
            << "\n\n\tThe date is Str "
-           << "\n\tThe day is "   << date.sday      
-           << "\n\tThe month is " << date.smonth
-           << "\n\tThe year is "  << date.syear
-           << "\n\tThe date String Format "  << date.toString() 
+           << "\n\tdate time..............: " << date.dateTime()
+           << "\n\tThe day is.............: " << date.sday      
+           << "\n\tThe month is...........: " << date.smonth
+           << "\n\tThe year is............: " << date.syear
+           << "\n\tThe date String Format.: " << date.toString() 
            << "\n\n";
  return 0;   
 }
