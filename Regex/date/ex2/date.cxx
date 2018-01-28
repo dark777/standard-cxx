877d07f81f7d65ff943ceace32e061f201743933
Jean Zonta <wiki.anon@yahoo.com.br>
2018-01-28 08:20:31 -0200

    Sources code C/C++

--- Regex/date/ex2/date.cxx
+++ Regex/date/ex2/date.cxx
@@ -15,8 +15,10 @@ struct date
  bool isDate()
  {
   std::smatch date_smatch;
+  
   //regex valida datas e anos bisextos nos formatos yyyy/[A|a]/dd ou yyyy/m/dd | yyyy.[A|a].dd ou yyyy.m.dd | yyyy-[A|a]-dd ou yyyy-m-dd
-   const std::regex pattern("^(?:\\d{4}(?:/|.|-)(?:(?:(?:(?:0[13578]|1[02]|(?:jan|mar|may|jul|aug|oct|dec|Jan|Mar|May|Jul|Aug|Oct|Dec))(?:/|.|-)(?:0[1-9]|[1-2][0-9]|3[01]))|(?:(?:0[469]|11|(?:apr|jun|sep|nov|Apr|Jun|S‌​ep|Nov))(?:/|.|-)(?:0[1-9]|[1-2][0-9]|30))|(?:(?:02|feb|Feb)(?:/|.|-)(?:0[1-9]|1[0-9]|2[0-8]))))|(?:(?:\\d{2}(?:0[48]|[2468][048]|[13579][26]))|(?:(?:[02468][048])|[13579][26])00)(?:/|.|-)(?:02|feb|Feb)(?:/|.|-)29)$");
+  const std::regex pattern("^(?:\\d{4}(?:/|.|-)(?:(?:(?:(?:0[13578]|1[02]|(?:jan|mar|may|jul|aug|oct|dec|Jan|Mar|May|Jul|Aug|Oct|Dec))(?:/|.|-)(?:0[1-9]|[1-2][0-9]|3[01]))|(?:(?:0[469]|11|(?:apr|jun|sep|nov|Apr|Jun|S‌​ep|Nov))(?:/|.|-)(?:0[1-9]|[1-2][0-9]|30))|(?:(?:02|feb|Feb)(?:/|.|-)(?:0[1-9]|1[0-9]|2[0-8]))))|(?:(?:\\d{2}(?:0[48]|[2468][048]|[13579][26]))|(?:(?:[02468][048])|[13579][26])00)(?:/|.|-)(?:02|feb|Feb)(?:/|.|-)29)$");
+ 
   return std::regex_match(this._date, date_smatch, pattern);
  }
  
