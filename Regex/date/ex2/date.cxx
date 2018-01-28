8bb7cfd1769a489cf0ab0cbaf9266cf70f0b9277
Jean Zonta <wiki.anon@yahoo.com.br>
2018-01-28 06:41:02 -0200

    Sources code C/C++

--- Regex/date/ex2/date.cxx
+++ Regex/date/ex2/date.cxx
@@ -15,8 +15,8 @@ struct date
  bool isDate()
  {
   std::smatch date_smatch;
-  //regex valida datas a partir de 1904 e anos bisextos nos formatos 0000-string-00 ou 0000-00-00 | 0000.string.00 ou 0000.00.00 | 0000/string/00 ou 0000/00/00
-  const std::regex pattern("^(?:\\d{4}[.-/](?:(?:(?:(?:0[13578]|1[02]|Jan|Mar|May|Jul|Aug|Oct|Dec)[.-/](?:0[1-9]|1[0-9]|2[0-9]|3[0|1]))|(?:(?:0[469]|11|Apr|Jun|S‌​ep|Nov)[.-/](?:0[1-9]|[1-2][0-9]|30))|(?:0[2]|Feb[.-/](?:0[1-9]|1[0-9]|2[0-8]))))|(?:(?:\\d{2}(?:0[48]|[2468][04‌​8]|[13579][26]))|(?:‌​(?:[02468][048])|[13‌​579][26])00)[.-/]Feb[.-/]29)$");
+  //regex valida datas a partir de 1904 e anos bisextos nos formatos 0000-string-00 ou 0000-00-00 | 0000.string.00 ou 0000.00.00 | 0000/string/00 ou 0000/00/00                                                                               //O problema esta aqui
+  const std::regex pattern("^(?:\\d{4}[-|.|/](?:(?:(?:(?:0[13578]|1[02]|Jan|Mar|May|Jul|Aug|Oct|Dec)[-|.|/](?:0[1-9]|1[0-9]|2[0-9]|3[0|1]))|(?:(?:0[469]|11|Apr|Jun|S‌​ep|Nov)[-|.|/](?:0[1-9]|[1-2][0-9]|30))|(?:(?:02|feb|Feb)[-|.|/](?:0[1-9]|1[0-9]|2[0-8]))))|(?:(?:\\d{2}(?:0[48]|[2468][04‌​8]|[13579][26]))|(?:‌​(?:[02468][048])|[13‌​579][26])00)[-|.|/](?:02|feb|Feb)[-|.|/]29)$");
    
   return std::regex_match(this._date, date_smatch, pattern);
  }
