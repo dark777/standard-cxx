4cd1a0c16c314e545b8320176ae769694b1d318b
Jean Zonta <wiki.anon@yahoo.com.br>
2018-01-29 17:46:56 -0200

    Sources code C/C++

--- HerancaMultipla/Ex1.cxx
+++ HerancaMultipla/Ex1.cxx
@@ -1,19 +1,11 @@
 #include <iostream>
 
-class Test1
+class Test4
 {
  public:
- Test1(){}
+ Test4(){}
  
- ~Test1(){ std::cout<<"\n\tI am class Test1.\n"; }
-};
-
-class Test2
-{
- public:
- Test2(){}
- 
- ~Test2(){ std::cout<<"\n\tI am class Test2.\n"; }
+ ~Test4(){ std::cout<<"\n\tI am class Test4.\n"; }
 };
 
 class Test3
@@ -24,22 +16,30 @@ class Test3
  ~Test3(){ std::cout<<"\n\tI am class Test3.\n"; }
 };
 
-class Test4: public Test1, public Test2, public Test3
+class Test2
+{
+ public:
+ Test2(){}
+ 
+ ~Test2(){ std::cout<<"\n\tI am class Test2.\n"; }
+};
+
+class Test1: public Test4, public Test3, public Test2
 {
  public:    
- Test4(){}
- ~Test4(){ std::cout<<"\n\tI am class Test4.\n";}
+ Test1(){}
+ ~Test1(){ std::cout<<"\n\tI am class Test1.\n";}
  void show();
 };
 
-void Test4::show()
+void Test1::show()
 {
- Test4(); 
+ Test1();
 }
      
 int main()
 {
- Test4 teste;
+ Test1 teste;
  teste.show();
  return 0;
-} 
+}
