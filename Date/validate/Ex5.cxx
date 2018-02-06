e59497a3b8eeae50f463cc3f14e01b957a726495
Jean Zonta <wiki.anon@yahoo.com.br>
2018-02-05 22:02:48 -0200

    Sources code C/C++

---
+++ Date/validate/Ex5.cxx
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+
+int main ()
+{
+  const char *str = "15-08-2012";
+  struct tm tm; 
+  if (strptime (str, "%d-%m-%Y", &tm) == NULL)
+  {
+    /* Bad format !! */
+  }
+  char buffer [80];
+  strftime (buffer, 80, "Day is %a", &tm);
+  puts (buffer);    
+  return 0;
+}
+
+ 
