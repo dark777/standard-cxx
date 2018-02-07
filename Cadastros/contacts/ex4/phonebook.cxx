66c1549b11e9c8c8d5938a6a3329b80635a9ac01
Jean Zonta <wiki.anon@yahoo.com.br>
2018-02-07 17:10:34 -0200

    Sources code C/C++

--- Cadastros/contacts/ex4/phonebook.cxx
+++ Cadastros/contacts/ex4/phonebook.cxx
@@ -2,30 +2,24 @@
 //===============================
 
 #include <iostream>
-using namespace std;
 
 
 //prototypes
 void printline(char, int);
-bool name_valid(string);
-bool mob_valid(string);
+bool name_valid(std::string);
+bool mob_valid(std::string);
 
-class contact
+struct contact
 {
-    string name;
-    string mob;
-        
-    public:
-    	//Initialize the contact by a default value
-        contact(): name(""), mob("")
-        {}
+     //Initialize the contact by a default value
+        contact(): name(""), mob(""){}
         
         // Shows all contacts
         bool show()
         {
             if(name != "")
             {
-                cout << name << "\t" << mob << endl;
+                std::cout << name << "\t" << mob << std::endl;
                 return 1; //Indicates success
             }
             else
@@ -33,11 +27,11 @@ class contact
         }
         
         //Search
-        bool show(string search_term)
+        bool show(std::string search_term)
         {
             if(search_term == name)
             {
-                cout << name << "\t" << mob << endl;
+                std::cout << name << "\t" << mob << std::endl;
                 return 1;
             }
             else
@@ -45,7 +39,7 @@ class contact
         }
         
         //Checks whether the name exists or not
-        bool name_exists(string tname)
+        bool name_exists(std::string tname)
         {
             if(tname == name)
                 return 1;
@@ -54,7 +48,7 @@ class contact
         }
         
         //The contact object is initialized by valid values
-        bool add(string new_name, string new_mob)
+        bool add(std::string new_name, std::string new_mob)
         {
             if(name=="")
             {
@@ -68,11 +62,11 @@ class contact
         }
         
         //Edits the contact details
-        bool edit(string);
+        bool edit(std::string);
         
         //Sets the contact details to default values
         //That is, the contact details are thus erased
-        bool erase(string new_name)
+        bool erase(std::string new_name)
         {
             if(new_name==name)
             {       
@@ -83,54 +77,59 @@ class contact
             else
                 return 0;
         }
+  private:
+   std::string name;
+   std::string mob;
 };
 
-
 //Edits the contact
-bool contact :: edit(string new_name)
+bool contact::edit(std::string new_name)
 {
-    string new_mob;
-    if(new_name==name)
-    {
-        cout << "Enter new name: "; cin >> new_name;
-        cout << "Enter new mobile no: "; cin >> new_mob;
+ std::string new_mob;
+ 
+ if(new_name == name)
+  {
+   std::cout << "Enter new name: ";
+   getline(std::cin,new_name);
         
-        name = new_name;
-        mob = new_mob;
-        return 1;
-    }
-    else
-        return 0;
+   std::cout << "Enter new mobile no: ";
+   getline(std::cin,new_mob);
+        
+   name = new_name;
+   mob = new_mob;
+   
+   return 1;
+  }
+  else
+  return 0;
 }
 
 
 
 int main()
 {
-    contact person[100];
+ contact person[100];
             
-    string temp_name, temp_mob;
-    int choice, i, counter;
-    bool flag;
-    bool cancel_flag;
+ std::string temp_name, temp_mob;
+ int choice, counter;
+ bool flag;
+ bool cancel_flag;
     
-    cout << "**** PHONEBOOK ******" << endl;
-    
+    std::cout << "\n\t**** PHONEBOOK ****";
     
     do
     {   
-        cout << "\n\n\n";
-        printline('-', 20);
-        cout << "0. Show contacts" << endl
-        << "1. Add Contact" << endl
-        << "2. Edit Contact" << endl
-        << "3. Delete Contact" << endl
-        << "4. Search" << endl
-        << "5. Quit" << endl << endl
-        << "Your choice...";
-        cin >> choice;
+        std::cout << "\n\t";
+        printline('-', 19);
+        std::cout << "\n\t0. Show contacts"
+                     "\n\t1. Add Contact"
+                     "\n\t2. Edit Contact"
+                     "\n\t3. Delete Contact"
+                     "\n\t4. Search"
+                     "\n\t5. Quit"
+                     "\n\tYour choice...";
+        std::cin >> choice;
         
-        system("cls");
         printline('-', 20);
         cancel_flag = 0;
         flag = 0;
@@ -139,200 +138,188 @@ int main()
         switch(choice)
         {
             case 0: 
-                cout << "Showing Contacts" << endl;
+                std::cout << "\n\tShowing Contacts\n";
                 printline('-', 20);
                 
-                for(i=0; i<100; i++)
+                for(int i=0; i<100; i++)
                     if(person[i].show())
                         flag = 1;
                 
                 if(!flag)
-                    cout << "No contacts found!" << endl;
+                    std::cout << "\n\tNo contacts found!\n";
                 break;
                 
             case 1:
-                cout << "Add New Contact\t\t\t\tpress $ to cancel" << endl;
+                std::cout << "\n\tAdd New Contact\t\t\t\tpress $ to cancel\n";
                 printline('-', 20);
                 counter = 0;
                 
                 //Loop until correct name and mobile number are entered
                 do
                 {
-                    flag = 0;
-                    if(counter)
-                        cout << "Try again\t\t\t\tpress $ to cancel" 
-						<< endl;
-						
-                    //counts how many times the do-while loop executes
-					counter++; 
+                 flag = 0;
+                 
+                 if(counter)std::cout << "\n\tTry again\t\t\t\tpress $ to cancel\n"; 
+                      
+                 //counts how many times the do-while loop executes
+                 counter++; 
                         
-                    cout << "Name: "; cin >> temp_name;
+                 std::cout << "Name: ";
+                 getline(std::cin,temp_name);
+                 //Cancel operation
+                 
+                 if(temp_name=="$")
+                  {
+                   cancel_flag = 1;
+                   break;
+                  }
+                 
+                 std::cout << "Mobile No.: ";
+                 getline(std::cin,temp_mob);
                     
-                    //Cancel operation
-                    if(temp_name=="$")
-                    {
-                        cancel_flag = 1;
-                        break;
-                    }
-                    cout << "Mobile No.: "; cin >> temp_mob;
+                 //Cancel operation
+                 if(temp_mob=="$")
+                  {
+                   cancel_flag = 1;
+                   break;
+                  }
                     
-                    //Cancel operation
-                    if(temp_mob=="$")
-                    {
-                        cancel_flag = 1;
-                        break;
-                    }
+                  //Check whether name exists
+                  for(int i=0; i<100; i++)
+                  if(person[i].name_exists(temp_name))
+                   {
+                    std::cout << "\n\tThe name you entered is already there" 
+                                 "in the phonebook, enter a different name.\n";
+                    flag = 1;
+                    break;
+                   }
                     
-                    //Check whether name exists
-                    for(i=0; i<100; i++)
-                        if(person[i].name_exists(temp_name))
-                        {
-                            cout << "The name you entered is already there" 
-							"in the phonebook, enter a different name." 
-							<< endl;
-                            flag = 1;
-                            break;
-                        }
-                    
-                }while(!name_valid(temp_name) || 
-								flag ||
-						!mob_valid(temp_mob));
+                }while(!name_valid(temp_name) || flag || !mob_valid(temp_mob));
                 
                 if(cancel_flag)
                 {
-                    system("cls");
-                    break;
+                 break;
                 }
-            
                 
                 //This code adds the contact to phonebook    
-                for(i=0; i<100; i++)
-                    if(person[i].add(temp_name, temp_mob))
-                    {
-                        cout << "Contact added successfully!" << endl;
-                        flag = 1;
-                        break;
-                    }
+                for(int i=0; i<100; i++)
+                 if(person[i].add(temp_name, temp_mob))
+                  {
+                   std::cout << "\n\tContact added successfully!" << std::endl;
+                   flag = 1;
+                   break;
+                  }
                 
                 if(!flag)
-                    cout << "Memory full! Delete some contacts first." 
-					<< endl;
+                 std::cout << "\n\tMemory full! Delete some contacts first.\n";
                 break;
                 
             case 2:
-                cout << "Enter a contact name to edit:" 
-				"\t\t\t\tpress $ to cancel\n";
-				 cin >> temp_name;
+                std::cout << "\n\tEnter a contact name to edit: "
+                             "\t\t\t\tpress $ to cancel\n";
+                             std::cin >> temp_name;
                 
                 //Cancel Operation
                 if(temp_name=="$")
                 {
-                    system("cls");
-                    break;
+                 break;
                 }
                 
-                for(i=0; i<100; i++)
-                    if(person[i].edit(temp_name))
-                    {
-                        cout << "Edited Successfully!" << endl;
-                        flag = 1;
-                        break;
-                    }
-                
-                if(!flag)
-                    cout << "Contact name not found!" << endl;
-                break;
+                for(int i=0; i<100; i++)
+                 if(person[i].edit(temp_name))
+                  {
+                   std::cout << "\n\tEdited Successfully!\n";
+                   flag = 1;
+                   break;
+                  }
+                 
+                 if(!flag)
+                 std::cout << "\n\tContact name not found!\n";
+                 break;
                 
             case 3:
                 do
                 {
-                    if(counter)
-                        cout << "Try again" << endl;
-                    counter++;
-                    cout << "Enter a contact name to delete:" 
-					"\t\t\tpress $ to cancel\n"; 
-					cin >> temp_name;
+                 if(counter)
+                  std::cout << "Try again\n";
+                  counter++;
+                  
+                  std::cout << "\n\tEnter a contact name to delete:" 
+                               "\t\t\tpress $ to cancel\n";
+                  getline(std::cin,temp_name);
                 
-                    //Cancel Operation
-                    if(temp_name=="$")
-                    {
-                        system("cls");
-                        break;
-                    }
-                
-                
-                    //Final Confirmation
-                    for(i=0; i<100; i++)
-                    if(person[i].name_exists(temp_name))
-                    {
-                        flag = 1;
-                        cout << "Are you sure you want to delete? (1/0)" 
-						<< endl;
-                        int yes;
-                        cin >> yes;
-                        if(!yes)
-                        {
-                            system("cls");
-                            cancel_flag = 1;
-                        }
-                        break;
-                    }
-                
-                    if(!flag)
-                        cout << "Contact name not found!" << endl;
+                  //Cancel Operation
+                  if(temp_name=="$")
+                   {
+                    break;
+                   }
+
+                  //Final Confirmation
+                  for(int i=0; i<100; i++)
+                  if(person[i].name_exists(temp_name))
+                   {
+                    int yes; 
+                    flag = 1;
+                    std::cout << "\n\tAre you sure you want to delete? (1/0)\n";
+                    std::cin >> yes;
                     
-                    if(cancel_flag)
-                        break;
+                    if(!yes)
+                     {
+                      cancel_flag = 1;
+                     }
+                    break;
+                   }
+                   
+                  if(!flag)std::cout << "\n\tContact name not found!\n";
+                    
+                  if(cancel_flag)break;
                 
-                    // This code deletes the contact
-                    if(flag)
-                    {
-                        for(i=0; i<100; i++)
-                            if(person[i].erase(temp_name))
-                            {
-                                cout << "Deleted successfully!" << endl;
-                                break;
-                            }
-                    }
+                  // This code deletes the contact
+                  if(flag)
+                   {
+                    for(int i=0; i<100; i++)
+                    if(person[i].erase(temp_name))
+                     {
+                      std::cout << "\n\tDeleted successfully!\n";
+                      break;
+                     }
+                   }
                     
                 }while(!flag);
                 break;
                 
             case 4:
-                do
-                {
-                    if(counter)
-                        cout << "Try again" << endl;
-                    counter++;
-                    cout << "Search a name: \t\t\t\tpress $ to cancel\n";
-					 cin >> temp_name;
-                
+                do{
+                   if(counter)std::cout << "\n\tTry again\n";
+                   counter++;
+                   
+                   std::cout << "Search a name: \t\t\t\tpress $ to cancel\n";
+                   std::cin >> temp_name;
+                   
                     //Cancel Operation
-                    if(temp_name=="$")
+                   if(temp_name=="$")
                     {
-                        system("cls");
-                        break;
+                     break;
                     }
-                
-                    for(i=0; i<100; i++)
-                        if(person[i].show(temp_name))
-                        {
-                            flag = 1;
-                            break;
-                        }
-    
-                    if(!flag)
-                        cout << "Contact name not found" << endl;
+                    
+                    for(int i=0; i<100; i++)
+                     if(person[i].show(temp_name))
+                      {
+                       flag = 1;
+                       break;
+                      }
+                      
+                   if(!flag)std::cout << "\n\tContact name not found\n";
                 }while(!flag);
                     
                 break;
                 
             case 5:
                 return 0;
-                break;
-            
+                break;  
         }
-    } while(1);
+        
+    }while(1);
     
     getchar();
     return 0;
@@ -342,45 +329,45 @@ int main()
 void printline(char ch, int size)
 {
     for(int i=0; i<size; i++)
-        cout << ch;
-    cout << "\n";
+     std::cout << ch;
+      std::cout << "\n";
 }
 
 
 //Contact name validation
-bool name_valid(string tname)
+bool name_valid(std::string tname)
 {
-    
     if(tname.size()>20)
     {
-        cout << "Invalid Name!\nEnter a name within 20 characters!" 
-		<< endl;
-        return 0;
-    }
-    else if(tname == "")
-    {
-        cout << "Invalid Name!\nName cannot be blank!" << endl;
-        return 0;
+     std::cout << "\n\tInvalid Name!\n\tEnter a name within 20 characters!\n";
+     return 0;
     }
     else
-        return 1;
+    if(tname == "")
+    {
+     std::cout << "\n\tInvalid Name!\n\tName cannot be blank!\n";
+     return 0;
+    }
+    else
+    return 1;
 }
 
 //mobile number validation
-bool mob_valid(string tmob)
+bool mob_valid(std::string tmob)
 {
-    if(tmob.size()>13 || tmob.size()<10)
-    {
-        cout << "Invalid mobile no.\nEnter a no." 
-		"between 10 and 13 digits" << endl;
-        return 0;
-    }
-    else if(tmob == "")
-    {
-        cout << "Invalid mobile no.\nMobile" 
-		"no cannot be blank" << endl;
-        return 0;
-    }
-    else
-        return 1;
+ if(tmob.size() > 13 || tmob.size() < 10)
+  {
+   std::cout << "\n\tInvalid mobile no.\n\tEnter a no." 
+                "between 10 and 13 digits\n";
+   return 0;
+  }
+  else 
+  if(tmob == "")
+  {
+   std::cout << "\n\tInvalid mobile no.\n\tMobile" 
+                "no cannot be blank\n";
+   return 0;
+  }
+  else
+  return 1;
 }
