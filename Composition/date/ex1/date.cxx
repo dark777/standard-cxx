2cc2acf2775cc86b54783eedc88236c0a16c9567
Jean Zonta <wiki.anon@yahoo.com.br>
2018-02-08 17:19:52 -0200

    Sources code C/C++

--- Composition/date/ex1/date.cxx
+++ Composition/date/ex1/date.cxx
@@ -8,7 +8,6 @@ struct Date
  
  std::string sday="";
  std::string smonth="";
- std::string syear="";
  
    Date()
    {
@@ -20,10 +19,6 @@ struct Date
     if(smonth.length() == 1)
     smonth.insert(0,"0");
     
-    syear = std::to_string(now->tm_year+1900);
-    if(syear.length() == 1)
-    syear.insert(0,"0");
-    
     day = std::to_string(now->tm_mday);
     month = std::to_string(now->tm_mon+1);
     year = std::to_string(now->tm_year+1900);
@@ -31,23 +26,32 @@ struct Date
    
    ~Date()
     {      
-     if(sday.length() != 0)sday.clear();
-     if(smonth.length() != 0)smonth.clear();     
-     if(syear.length() != 0)syear.clear();
+     sday.clear();
+     smonth.clear();
      
-     if(day.length() != 0)day.clear();
-     if(month.length() != 0)month.clear();
-     if(year.length() != 0)year.clear();
+     day.clear();
+     month.clear();
+     year.clear();
     }
    
    std::string toInt()
    {
-    return std::string(day+"/"+month+"/"+year);
+    return day+"/"+month+"/"+year;
    }
    
    std::string toString()
    {
-    return std::string(sday+"/"+smonth+"/"+syear);
+    return sday+"/"+smonth+"/"+year;
+   }
+   
+   std::string toInt(std::string sep)
+   {
+    return day+sep+month+sep+year;
+   }
+   
+   std::string toString(std::string sep)
+   {
+    return sday+sep+smonth+sep+year;
    }
    
    std::string dateTime()
@@ -67,16 +71,18 @@ struct Date
 int main()
 {
  std::cout << "\n\tThe date is Int "
-           << "\n\tThe day is.............: " << date.day      
-           << "\n\tThe month is...........: " << date.month
-           << "\n\tThe year is............: " << date.year
-           << "\n\tThe date Int Format....: " << date.toInt()
-           << "\n\n\tThe date is Str "
-           << "\n\tdate time..............: " << date.dateTime()
-           << "\n\tThe day is.............: " << date.sday      
-           << "\n\tThe month is...........: " << date.smonth
-           << "\n\tThe year is............: " << date.syear
-           << "\n\tThe date String Format.: " << date.toString() 
-           << "\n\n";
+           << "\n\tThe day is..............: " << date.day      
+           << "\n\tThe month is............: " << date.month
+           << "\n\tThe year is.............: " << date.year
+           << "\n\tThe date Int Format.....: " << date.toInt()
+           << "\n\tThe date Int Format Sep.: " << date.toInt("-")
+	   << "\n\n\tThe date is Str "
+           << "\n\tdate time..................: " << date.dateTime()
+           << "\n\tThe day is.................: " << date.sday      
+           << "\n\tThe month is...............: " << date.smonth
+           << "\n\tThe year is................: " << date.year
+           << "\n\tThe date String Format.....: " << date.toString() 
+           << "\n\tThe date String Format Sep.: " << date.toString("-")
+	   << "\n\n";
  return 0;   
 }
