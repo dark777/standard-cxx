f1f9e0ba8168044d1c95cc729915523cb15bd399
Jean Zonta <wiki.anon@yahoo.com.br>
2018-02-12 10:56:00 -0200

    Sources code C/C++

--- postgre_connect/examples/select.cxx
+++ postgre_connect/examples/select.cxx
@@ -14,6 +14,20 @@ int main(int argc, char* argv[])
        return 1;
       }
       
+      // Create a non-transactional object.
+      pqxx::nontransaction non_obj(login);
+      
+      // Create SQL statement stringstream;
+      std::stringstream contsql;
+      
+      contsql<<"select *from person where person_id = (select count(*) from person);";
+      
+      // Execute SQL query 
+      pqxx::result res1(non_obj.exec(contsql));
+      
+      for (pqxx::result::const_iterator num1 = res1.begin(); num1 != res1.end(); ++num1)
+      std::cout << "\n\tNum Rows.: " << num1[0].as<int>()<<"\n";
+      
       std::cout<<"\n\tQual Usuario deseja listar? ";
       std::cin>>argc;
       
@@ -21,28 +35,25 @@ int main(int argc, char* argv[])
       std::stringstream sql;
       sql << "select *from person where person_id=" << argc;
       
-      
       // Create SQL statement const char*
       //const char * sql; 
       //sql = "select *from person where person_id=1";
-
-      /* Create a non-transactional object. */
-      pqxx::nontransaction non_obj(login);
       
-      /* Execute SQL query */
-      pqxx::result res(non_obj.exec(sql));
+      // Execute SQL query
+      pqxx::result res2(non_obj.exec(sql));
       
-      /* List down all the records */
-      for (pqxx::result::const_iterator c = res.begin(); c != res.end(); ++c)
-       std::cout << "\n\tIdPerson.: " << c[0].as<int>()
-                 << "\n\tName.....: " << c[1].as<std::string>()
-                 << "\n\tAge......: " << c[2].as<int>()
-                 << "\n\tStatus...: " << c[3].as<std::string>()
-                 << "\n\tIdFaz....: " << c[4].as<int>()
-                 << "\n\tDate.....: " << c[5].as<std::string>()
-                 << "\n\n";
+      // List down all the records
+      for (pqxx::result::const_iterator num2 = res2.begin(); num2 != res2.end(); ++num2)
+      std::cout << "\n\tIdPerson.: " << num2[0].as<int>()
+                << "\n\tName.....: " << num2[1].as<std::string>()
+                << "\n\tAge......: " << num2[2].as<int>()
+                << "\n\tStatus...: " << num2[3].as<std::string>()
+                << "\n\tIdFaz....: " << num2[4].as<int>()
+                << "\n\tDate.....: " << num2[5].as<std::string>()
+                << "\n\n";
       
-     std::cout << "\n\tOperation done successfully\n\n";
+      std::cout << "\n\tOperation done successfully\n\n";
+          
      login.disconnect();
     }
     catch(const std::exception &e)
