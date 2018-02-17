84e9a6883abb00c714419bcc8aa6f0402505935a
Jean Zonta <wiki.anon@yahoo.com.br>
2018-02-17 17:49:55 -0200

    Sources code C/C++

--- Regex/date/ex2/date.cxx
+++ Regex/date/ex2/date.cxx
@@ -21,7 +21,7 @@ struct date
                            R"((?:(?:(0?[1-9]|1\d|2[0-8])([-/.])(0?[1-9]|1[0-2]|j(?:an|u[nl])|ma[ry]|a(?:pr|ug)|sep|oct|nov|dec|feb)|(29|30)([-/.])(0?[13-9]|1[0-2]|j(?:an|u[nl])|ma[ry]|a(?:pr|ug)|sep|oct|nov|dec)|(31)([-/.])(0?[13578]|1[02]|jan|ma[ry]|jul|aug|oct|dec))(?:\2|\5|\8)(0{2,3}[1-9]|0{1,2}[1-9]\d|0?[1-9]\d{2}|[1-9]\d{3})|(29)([-/.])(0?2|feb)\12(\d{1,2}(?:0[48]|[2468][048]|[13579][26])|(?:0?[48]|[13579][26]|[2468][048])00)))",
                            std::regex_constants::icase
                           );
-    */
+  */
   return std::regex_match(_date, date_smatch, pattern);
  }
  
