a6cc2b9eff6f8b155adedee99527db567e796901
Jean Zonta <wiki.anon@yahoo.com.br>
2018-02-19 01:39:55 -0300

    Sources code C/C++

--- Date/validate/getdate.cxx
+++ Date/validate/getdate.cxx
@@ -23,6 +23,33 @@ struct GetDate
    return timeInfo.tm_mday;
   }
   
+  std::string diaSemana()
+  {
+   //Algoritmo para descobrir o dia da semana
+   int a = ((12 - Month()) / 10);
+   int b = (Year() - a);
+   int c = (Month() + (12 * a));
+   int d = (b/100);
+   int e = (d/4);
+   int f = ((2-d) + e);
+   int g = int(365.25 * b);
+   int h = int(30.6001 * (c + 1));
+   int i = int((f + g) + (h + Day()) + 5);
+   int j = int(i % 7); //Resto de I por 7, onde 0=Saturday, 1=Sunday, 2=Monday, 3=Tuesday, 4=Wednesday, 5=Thursday, 6=Friday
+
+   //Testa o resultado e retorna
+   switch(j)
+    {
+     case 0: return "Saturday";
+     case 1: return "Sunday";
+     case 2: return "Monday";
+     case 3: return "Tuesday";
+     case 4: return "Wednesday";
+     case 5: return "Thursday";
+     case 6: return "Friday";
+    }
+  }
+  
   private:
   time_t timeval = time(NULL);
   tm timeInfo = *localtime(&timeval);
@@ -39,6 +66,6 @@ int main()
  month = get.Month();
  year = get.Year();
  
- std::cout<<"\n\t"<<day<<"/"<<month<<"/"<<year<<"\n\n";
+ std::cout<<"\n\tweekday: "<<get.diaSemana()<<" "<<day<<"/"<<month<<"/"<<year<<"\n\n";
  return 0;
 }
