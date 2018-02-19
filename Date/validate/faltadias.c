217f8dcd9284c3c8669e774785f48de0fe10f9dd
Jean Zonta <wiki.anon@yahoo.com.br>
2018-02-19 10:35:59 -0300

    Sources code C/C++

--- Date/validate/faltadias.c
+++ Date/validate/faltadias.c
@@ -19,21 +19,21 @@ int main ()
 {
  int falta_dias = 0;
  int dia, mes, ano;
- int dias_mes[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+ int dias_mes[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
  
  do{
     printf("Digite uma data no formato dd/mm/yyyy: ");
     scanf("%d/%d/%d", &dia, &mes, &ano);
     
-    if(dia>31 || dia<1)printf("\n\tDia %d invalido.!!\n\tDigite um dia de 01 a 31\n",dia);
+    if(dia > 31 || dia < 1)printf("\n\tDia %d invalido.!!\n\tDigite um dia de 01 a 31\n",dia);
     
-    if(mes>12 || mes <1)printf("\n\tMes %d invalido.!!\n\tDigite um mes de 01 a 12\n",mes);
+    if(mes > 12 || mes < 1)printf("\n\tMes %d invalido.!!\n\tDigite um mes de 01 a 12\n",mes);
     
-   }while((dia>31 || dia<1) || (mes>12 || mes <1));
+   }while((dia > 31 || dia < 1) || (mes > 12 || mes < 1));
    
-   dias_mes[2] = (ano%4 == 0 || ano%400 == 0 && ano%100 != 0) ? 29 : 28;
+   dias_mes[1] = (ano%4 == 0 || ano%400 == 0 && ano%100 != 0) ? 29 : 28;
    
-   for(int i = mes; i<13; i++)
+   for(int i = mes; i<12; i++)
    falta_dias += dias_mes[i];
     
    printf("\n\nFaltam %d dias para terminar o ano %04d.\n\n", falta_dias,ano);
--- Date/validate/getdate.cxx
+++ Date/validate/getdate.cxx
@@ -2,28 +2,38 @@
 
 struct GetDate
 {
-  int Month()
-  {
-   if(&timeInfo == NULL)return -1;
-   else
-   return timeInfo.tm_mon+1;
-  }
-
-  int Year()
-  {
-   if(&timeInfo == NULL)return -1;
-   else
-   return timeInfo.tm_year+1900;
-  }
- 
-  int Day()
-  {
-   if(&timeInfo == NULL)return -1;
-   else
-   return timeInfo.tm_mday;
-  }
+  struct Weeks
+   {
+    const char* longDays[7]={"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+   }week;
    
-  int dayOfWeek(int day, int month, int year)
+  struct Months
+   {
+    const char *longMonths[12]={ "January","February","March","April","May","June","July","August","September","October","November","December" };
+   }month;
+   
+  int Month()
+   {
+    if(&timeInfo == NULL)return -1;
+    else
+    return timeInfo.tm_mon+1;
+   }
+  
+  int Year()
+   {
+    if(&timeInfo == NULL)return -1;
+    else
+    return timeInfo.tm_year+1900;
+   }
+   
+  int Day()
+   {
+    if(&timeInfo == NULL)return -1;
+    else
+    return timeInfo.tm_mday;
+   }
+   
+  int dayWeek(int day, int month, int year)
    {
     timeInfo.tm_year = year - 1900;
     timeInfo.tm_mon = month - 1;
@@ -32,50 +42,80 @@ struct GetDate
     mktime(&timeInfo);
     return timeInfo.tm_wday;
    }
-  
+   
+  std::string theMonth()
+   {
+    //Testa o resultado e retorna
+    switch(Month())
+     {
+      case January: return shortMonths[Month()-1]; break;
+      case February: return shortMonths[Month()-1]; break;
+      case March: return shortMonths[Month()-1]; break;
+      case April: return shortMonths[Month()-1]; break;
+      case May: return shortMonths[Month()-1]; break;
+      case June: return shortMonths[Month()-1]; break;
+      case July: return shortMonths[Month()-1]; break;
+      case August: return shortMonths[Month()-1]; break;
+      case September: return shortMonths[Month()-1]; break;
+      case October: return shortMonths[Month()-1]; break;
+      case November: return shortMonths[Month()-1]; break;
+      case December: return shortMonths[Month()-1]; break;
+     }
+     
+   }
+   
   std::string dayWeek()
-  {
-   //Algoritmo para descobrir o dia da semana
-   int a = ((12 - Month()) / 10);
-   int b = (Year() - a);
-   int c = (Month() + (12 * a));
-   int d = (b/100);
-   int e = (d/4);
-   int f = ((2-d) + e);
-   int g = int(365.25 * b);
-   int h = int(30.6001 * (c + 1));
-   int i = int((f + g) + (h + Day()) + 5);
-   int j = int(i % 7); //Resto de I por 7, onde 0=Saturday, 1=Sunday, 2=Monday, 3=Tuesday, 4=Wednesday, 5=Thursday, 6=Friday
-
-   //Testa o resultado e retorna
-   switch(j)
-    {
-     case 0: return "Saturday";
-     case 1: return "Sunday";
-     case 2: return "Monday";
-     case 3: return "Tuesday";
-     case 4: return "Wednesday";
-     case 5: return "Thursday";
-     case 6: return "Friday";
-    }
-  }
-  
+   {
+    //Algoritmo para pegar o dia da semana
+    int a = ((12 - Month()) / 10);
+    int b = (Year() - a);
+    int c = (Month() + (12 * a));
+    int d = (b/100);
+    int e = (d/4);
+    int f = ((2-d) + e);
+    int g = int(365.25 * b);
+    int h = int(30.6001 * (c + 1));
+    int i = int((f + g) + (h + Day()) + 5);
+    int j = int(i % 7); //Resto de I por 7, onde 0=Saturday, 1=Sunday, 2=Monday, 3=Tuesday, 4=Wednesday, 5=Thursday, 6=Friday
+   
+    //Testa o resultado e retorna
+    switch(j)
+     {
+      case Zero: return shortDays[j-1]; break;
+      case One: return shortDays[j-1]; break;
+      case Two: return shortDays[j-1]; break;
+      case Three: return shortDays[j-1]; break;
+      case Four: return shortDays[j-1]; break;
+      case Five: return shortDays[j-1]; break;
+      case Six: return shortDays[j-1]; break;
+     }
+   }
+   
   private:
+  
   time_t timeval = time(NULL);
+  
   tm timeInfo = *localtime(&timeval);
+  
+  enum { Zero, One, Two, Three, Four, Five, Six };
+  
+  const char* shortDays[7]={"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+  
+  const char *shortMonths[12]={ "Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec" };
+  
+  enum { January=1, February, March, April, May, June, July, August, September, October, November, December};
 }get;
 
 
 int main()
 {
- int day;
- int month;
- int year;
+ int day = get.Day();
+ int month = get.Month();
+ int year = get.Year();
  
- day = get.Day();
- month = get.Month();
- year = get.Year();
+ std::cout<<"\n\tshort Weekday: "<<get.dayWeek()<<" "<<day<<"/"<<get.theMonth()<<"/"<<year<<"\n";
  
- std::cout<<"\n\tweekday: "<<get.dayOfWeek(day,month,year)<<"º "<<get.dayWeek()<<" "<<day<<"/"<<month<<"/"<<year<<"\n\n";
+ std::cout<<"\n\tlong Weekday: "<<get.week.longDays[get.dayWeek(day,month,year)]<<" "<<day<<"/"<<get.month.longMonths[month-1]<<"/"<<year<<"\n\n";
+ 
  return 0;
 }
