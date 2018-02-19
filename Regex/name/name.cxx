e71eca6dbf784f7215d9cb0fdb98a8e83ccd55c1
Jean Zonta <wiki.anon@yahoo.com.br>
2018-02-19 17:57:36 -0300

    Sources code C/C++

--- Regex/name/name.cxx
+++ Regex/name/name.cxx
@@ -2,7 +2,7 @@
 #include <iostream>
 
 struct name
-{
+{  
    name(std::string nome): _nome(nome){}
    
    ~name()
@@ -15,8 +15,10 @@ struct name
     std::smatch name_smatch;
     
     const std::regex pattern(
-                             "((?:[á-úA-Za-z]+[[:space:]]+[á-úA-Za-z]{0,20})?)?" //valida nome e sobrenome
-                             "((?:[[:space:]]+[á-úA-Za-z]{0,10})?)?" // valida nome, nome composto e sobrenome
+                             "((?:[á-úA-Za-z]+[[:space:]]+[á-úA-Za-z.]{0,20})?)?" //valida nome e sobrenome
+                             "((?:[[:space:]]+[á-úA-Za-z.]{0,10})?)?" // valida nome, nome composto e sobrenome
+                             "((?:[[:space:]]+[á-úA-Za-z.]{0,10})?)?" // valida nome, 2 nomes composto e sobrenome
+                             "((?:[[:space:]]+[á-úA-Za-z.]{0,10})?)?" // valida nome, 3 nomes composto e sobrenome
                             );
     return std::regex_match(_nome, name_smatch, pattern);
    }
@@ -30,19 +32,16 @@ struct name
     std::string _nome;
 };
 
-
 int main()
-{    
- std::string strname;
-    
+{
  do{
     std::cout << "\n\tEnter full name: ";
-    getline(std::cin, strname);
-    
-    name(strname).print();
+    getline(std::cin, _nome);
+       
+    name(_nome).print();
     std::cout<<"\n";
-    
-   }while(!name(strname).isName());
-  
-  return 0;
+       
+   }while(!name(_nome).isName());
+ 
+ return 0;
 }
