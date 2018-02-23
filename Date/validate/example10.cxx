239fb5ec727d492274fa163578f9f8d12f6b6e30
Jean Zonta <wiki.anon@yahoo.com.br>
2018-02-23 01:20:35 -0300

    Sources code C/C++

--- Date/validate/example10.cxx
+++ Date/validate/example10.cxx
@@ -1,73 +1,82 @@
-#include <ctime>
-#include <fcntl.h>
-#include <unistd.h>
 #include <iostream>
-#include <sys/stat.h>
-#include <sys/types.h>
 
-std::string current_date (const std::string & format)
+std::string current_date(const std::string& format)
 {
-    time_t timer = time(NULL);
-
-    char temp[32] = "";
-
-    struct tm dtm;
-    if (format == "yyyy-mm-dd")
-    {
-        strftime (temp, sizeof(temp) - 1, "%Y-%m-%d", localtime_r(&timer, &dtm));
-    }
-    else if (format == "yy-mm-dd")
-    {
-        strftime (temp, sizeof(temp) - 1, "%y-%m-%d", localtime_r(&timer, &dtm));
-    }
-    else if (format == "yyyymmdd")
-    {
-        strftime (temp, sizeof(temp) - 1, "%Y%m%d", localtime_r(&timer, &dtm));
-    }
-    else if (format == "yymmdd")
-    {
-        strftime (temp, sizeof(temp) - 1, "%y%m%d", localtime_r(&timer, &dtm));
-    }
-
-    return temp;
+ tm dtm;
+ 
+ time_t timer = time(NULL);
+ 
+ char temp[32] = "";
+ 
+ if(format == "yyyy-mm-dd")
+  {
+   strftime(temp, sizeof(temp) - 1, "%Y-%m-%d", localtime_r(&timer, &dtm));
+  }
+ else 
+ if(format == "yy-mm-dd")
+  {
+   strftime(temp, sizeof(temp) - 1, "%y-%m-%d", localtime_r(&timer, &dtm));
+  }
+ else 
+ if(format == "yyyymmdd")
+  {
+   strftime(temp, sizeof(temp) - 1, "%Y%m%d", localtime_r(&timer, &dtm));
+  }
+ else 
+ if(format == "yymmdd")
+  {
+   strftime(temp, sizeof(temp) - 1, "%y%m%d", localtime_r(&timer, &dtm));
+  }
+  
+ return temp;
 }
 
-
-std::string current_time (const std::string & format)
+std::string current_time(const std::string& format)
 {
-    time_t timer = time(NULL);
-
-    char temp[32] = "";
-
-    struct tm dtm;
-    if (format == "HH:MM:SS")
-    {
-        strftime (temp, sizeof(temp) - 1, "%H:%M:%S", localtime_r(&timer, &dtm));
-    }
-    else if (format == "HH:MM")
-    {
-        strftime (temp, sizeof(temp) - 1, "%H:%M", localtime_r(&timer, &dtm));
-    }
-    else if (format == "HHMMSS")
-    {
-        strftime (temp, sizeof(temp) - 1, "%H%M%S", localtime_r(&timer, &dtm));
-    }
-    else if (format == "HHMM")
-    {
-        strftime (temp, sizeof(temp) - 1, "%H%M", localtime_r(&timer, &dtm));
-    }
-    
-    return temp;
+ tm dtm;
+ 
+ time_t timer = time(NULL);
+ 
+ char temp[32] = "";
+ 
+ if(format == "HH:MM:SS")
+  {
+   strftime(temp, sizeof(temp) - 1, "%H:%M:%S", localtime_r(&timer, &dtm));
+  }
+ else
+ if(format == "HH:MM")
+  {
+   strftime (temp, sizeof(temp) - 1, "%H:%M", localtime_r(&timer, &dtm));
+  }
+ else
+ if(format == "HHMMSS")
+  {
+   strftime(temp, sizeof(temp) - 1, "%H%M%S", localtime_r(&timer, &dtm));
+  }
+ else
+ if(format == "HHMM")
+  {
+   strftime(temp, sizeof(temp) - 1, "%H%M", localtime_r(&timer, &dtm));
+  }
+ return temp;
 }
 
-
-std::string exp_date (int days, int hours, int minutes, int seconds)
+std::string exp_date(int days, int hours, int minutes, int seconds)
 {
-    time_t t = time(NULL) + seconds + 60 * (minutes + 60 * (hours + 24 * days));
-
-    char date[256] = "";
-    struct tm dtm;
-    strftime (date, sizeof(date), "%A, %d-%b-%Y %H:%M:%S GMT", gmtime_r (&t, &dtm));
-
-    return date;
+ tm dtm;
+ 
+ time_t t = time(NULL) + seconds + 60 * (minutes + 60 * (hours + 24 * days));
+ 
+ char date[256] = "";
+ 
+ strftime(date, sizeof(date), "%A, %d-%b-%Y %H:%M:%S GMT", gmtime_r(&t, &dtm));
+ 
+ return date;
 } 
+
+int main()
+{
+ std::cout<<"\n\t"<<current_date("yyyy-mm-dd")<<" "<<current_time("HH:MM:SS")<<"\n\t"<<exp_date(34,12,35,59)<<"\n\n";
+ 
+ return 0;
+}
