c77aea93573f521f83f9b38036228e0aeb496d6b
Jean Zonta <wiki.anon@yahoo.com.br>
2018-02-27 16:58:22 -0300

    Sources code C/C++

--- Date/pascoa/pascoa1.cxx
+++ Date/pascoa/pascoa1.cxx
@@ -1,68 +1,82 @@
 #include <iostream>
-//http://www.webcid.com.br/calendario/2018/brasil
+/*
+.--------.-------------------------------.----------------------------------.
+|Formato |Britânico:                     |Americano:                        |
+|        |Dia - Mês - Ano                |Mês - Dia - Ano                   |
+|--------+-------------------------------+----------------------------------|
+|    A   | the Twenty-fourth of February,| February, the Twenty-fourth,2009 |
+|        |             2009              |                                  |
+|--------+-------------------------------+----------------------------------|
+|    B   |     24th February 2009        |     February 24th, 2009          |
+|    C   |      24 February 2009         |      February 24, 2009           |
+|    D   |          24/2/2009            |          2/24/2009               |
+|    E   |           24/2/09             |           2/24/09                |
+|    F   |          24/02/09             |          02/24/09                |
+'--------'-------------------------------'----------------------------------'
+*/
 
 int main()
 {
-
-time_t now = time(0);
-tm *ltm = localtime(&now);
-
-int anoatual = ltm->tm_year+1900;
-
-
-printf("O ano atual é %4d.", anoatual);
-
-int ano;
-
-printf("Digite o ano desejado para calcularmos o dia da páscoa: ");
-scanf("%d", &ano);
-
-int a=(ano%19);
-int b=int(ano/100);
-int c=(ano%100);
-int d=int(b/4);
-int e=(b%4);
-int f=int((b+8)/25);
-int g=int((b-f+1)/3);
-int h=((19*a+b-d-g+15)%30);
-int i=int(c/4);
-int k=(c%4);
-
-int l=((32+2*e+2*i-h-k)%7);
-int m=int((a+11*h+22*l)/451);
-
-int mes=int((h+l-7*m+114)/31); //esta ainda igual também
-
-char* mes1 = "Mês inválido"; //aqui nova variável para o texto do mes
-
-switch (mes){ //agora com switch em vez de ifs
-    case 1: mes1 = "Janeiro"; break;
-    case 2: mes1 = "Fevereiro"; break;
-    case 3: mes1 = "Março"; break;
-    case 4: mes1 = "Abril"; break;
-    case 5: mes1 = "Maio"; break;
-    case 6: mes1 = "Junho"; break;
-    case 7: mes1 = "Julho"; break;
-    case 8: mes1 = "Agosto"; break;
-    case 9: mes1 = "Setembro"; break;
-    case 10: mes1 = "Outubro"; break;
-    case 11: mes1 = "Novembro"; break;
-    case 12: mes1 = "Dezembro"; break;
-}
-
-int dia=((h+l-7*m+114)%31)+1;
-
-if(anoatual>ano)
-{
- printf("A pascoa caiu no dia: %d.",dia);
- printf("Do mês: %s", mes1);
-}
-else
-if(anoatual<ano)
-{
- printf("A pascoa ira cair no dia: %d.",dia);
- printf("Do mês: %s", mes1);
-}
-
- return 0;
+  time_t now = time(0);
+  tm *ltm = localtime(&now);
+  
+  int current_day(ltm->tm_mday);
+  int current_month(ltm->tm_mon+1);
+  int current_year(ltm->tm_year+1900);
+  
+  const char *months[12] = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+  
+  printf("\n\tThe current year is %4d\n",current_year);
+  
+  int year;
+  
+  printf("\n\tEnter the desired year to\n\tcalculate the day of Easter: ");
+  scanf("%d", &year);
+  
+  int a=int(year%19);
+  int b=int(year/100);
+  int c=int(year%100);
+  int d=int(b/4);
+  int e=int(b%4);
+  int f=int((b+8)/25);
+  int g=int((b-f+1)/3);
+  int h=((19*a+b-d-g+15)%30);
+  int i=int(c/4);
+  int k=int(c%4);
+  int l=((32+2*e+2*i-h-k)%7);
+  int m=int((a+11*h+22*l)/451);
+  int easter_month=int((h+l-7*m+114)/31);
+  int easter_day=int((h+l-7*m+114)%31)+1;
+ 
+ if(year > current_year)
+  {
+   printf("\n\tEaster will fall on %s %dth %4d.\n\n", months[easter_month-1],easter_day,year);
+  }
+ else
+ if(year < current_year)
+  {
+   printf("\n\tEaster fell on %dth %s %4d.\n\n",easter_day, months[easter_month-1],year);
+  }
+ else
+ if(year == current_year && current_month == easter_month && current_day == easter_day)
+  {
+   printf("\n\tToday %dth %s %04d is Easter Day",easter_day, months[easter_month-1], year);
+  }
+ else
+ if(year == current_year && current_month > easter_month)
+  {
+   printf("\n\tEaster fell on %dth %s %4d.\n\n",easter_day, months[easter_month-1],year);
+  }
+ else
+ if(year == current_year && current_month < easter_month)
+  {
+   printf("\n\tEaster will fall on %s %dth %4d.\n\n", months[easter_month-1],easter_day,year);
+  }
+ else
+ if(year == current_year && current_month == easter_month && current_day < easter_day)
+  {
+   printf("\n\tEaster will fall on %s %dth %4d.\n\n", months[easter_month-1],easter_day,year);
+  }
+    
+  return 0;
 }
--- Date/pascoa/pascoa2.cxx
+++ Date/pascoa/pascoa2.cxx
@@ -1,67 +1,85 @@
 #include <iostream>
-/*
-.--------.-------------------------------.----------------------------------.
-|Formato |Britânico:                     |Americano:                        |
-|        |Dia - Mês - Ano                |Mês - Dia - Ano                   |
-|--------+-------------------------------+----------------------------------|
-|    A   | the Twenty-fourth of February,| February, the Twenty-fourth,2009 |
-|        |             2009              |                                  |
-|--------+-------------------------------+----------------------------------|
-|    B   |     24th February 2009        |     February 24th, 2009          |
-|    C   |      24 February 2009         |      February 24, 2009           |
-|    D   |          24/2/2009            |          2/24/2009               |
-|    E   |           24/2/09             |           2/24/09                |
-|    F   |          24/02/09             |          02/24/09                |
-'--------'-------------------------------'----------------------------------'
-*/
 
 int main()
 {
-  time_t now = time(0);
-  tm *ltm = localtime(&now);
-  
-  int current_day(ltm->tm_mday);
-  int current_month(ltm->tm_mon+1);
-  int current_year(ltm->tm_year+1900);
-  
-  const char *months[12] = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
-  
-  printf("\n\tThe current year is %4d\n",current_year);
-  
-  int year;
-  
-  printf("\n\tEnter the desired year to\n\tcalculate the day of Easter: ");
-  scanf("%d", &year);
-  
-  int a=int(year%19);
-  int b=int(year/100);
-  int c=int(year%100);
-  int d=int(b/4);
-  int e=int(b%4);
-  int f=int((b+8)/25);
-  int g=int((b-f+1)/3);
-  int h=((19*a+b-d-g+15)%30);
-  int i=int(c/4);
-  int k=int(c%4);
-  int l=((32+2*e+2*i-h-k)%7);
-  int m=int((a+11*h+22*l)/451);
-  int month=int((h+l-7*m+114)/31);
-  int easter_day=(((h+l-7*m+114)%31)+1);
+ time_t now = time(0);
+ tm *ltm = localtime(&now);
  
-  if(current_year >= year && easter_day > current_day && current_month > month)
-   printf("\n\tEaster has fallen. !!\n\t%dth %s %04d\n\n", easter_day, months[month-1], year); //British date format
-  else
-  if(current_year <= year && easter_day < current_day && current_month < month)
-   printf("\n\tEaster will fall. !!\n\t%s %dth, %04d\n\n", months[month-1], easter_day, year); //American date format
-    
-  return 0;
+ int current_day(ltm->tm_mday);
+ int current_month(ltm->tm_mon+1);
+ int current_year(ltm->tm_year+1900);
+
+ printf("\n\tThe current year is %4d\n",current_year);
+  
+ int year;
+  
+ printf("\n\tEnter the desired year to\n\tcalculate the day of Easter: ");
+ scanf("%d", &year);
+ 
+ int a=(year%19);
+ int b=int(year/100);
+ int c=(year%100);
+ int d=int(b/4);
+ int e=(b%4);
+ int f=int((b+8)/25);
+ int g=int((b-f+1)/3);
+ int h=((19*a+b-d-g+15)%30);
+ int i=int(c/4);
+ int k=(c%4);
+ int l=((32+2*e+2*i-h-k)%7);
+ int m=int((a+11*h+22*l)/451);
+ int easter_month=int((h+l-7*m+114)/31);
+ int easter_day=int((h+l-7*m+114)%31)+1;
+ 
+ enum { January=1, February, March, April, May, June, July, August, September, October, November, December };
+
+ const char* months = "Month Invalid"; //aqui nova variável para o texto do easter_month
+
+ switch(easter_month)
+  {
+   case January:   months = "January";   break;
+   case February:  months = "February";  break;
+   case March:     months = "March";     break;
+   case April:     months = "April";     break;
+   case May:       months = "May";       break;
+   case June:      months = "June";      break;
+   case July:      months = "July";      break;
+   case August:    months = "August";    break;
+   case September: months = "September"; break;
+   case October:   months = "October";   break;
+   case November:  months = "November";  break;
+   case December:  months = "December";  break;
+  }
+ 
+ if(year > current_year)
+  {
+   printf("\n\tEaster will fall on %s %dth %4d.\n\n", months,easter_day,year);
+  }
+ else
+ if(year < current_year)
+  {
+   printf("\n\tEaster fell on %dth %s %4d.\n\n",easter_day, months,year);
+  }
+ else
+ if(year == current_year && current_month == easter_month && current_day == easter_day)
+  {
+   printf("\n\tToday %dth %s %04d is Easter Day",easter_day, months, year);
+  }
+ else
+ if(year == current_year && current_month > easter_month)
+  {
+   printf("\n\tEaster fell on %dth %s %4d.\n\n",easter_day, months,year);
+  }
+ else
+ if(year == current_year && current_month < easter_month)
+  {
+   printf("\n\tEaster will fall on %s %dth %4d.\n\n", months,easter_day,year);
+  }
+ else
+ if(year == current_year && current_month == easter_month && current_day < easter_day)
+  {
+   printf("\n\tEaster will fall on %s %dth %4d.\n\n", months,easter_day,year);
+  }
+  
+ return 0;
 }
-/*
- if(year == current_year && current_month == 4 && current_day == easter_day)
- printf("\n\tToday %dth %s %04d is Easter Day",easter_day, months[month-1], year); //British date format
- else
- if(year >= current_year && current_month <= 4 && (current_day < easter_day || current_day > easter_day))
- printf("\n\tEaster will fall. !!\n\t%s %dth, %04d\n\n", months[month-1], easter_day, year); //American date format
- else
-printf("\n\tEaster has fallen. !!\n\t%dth %s %04d\n\n", easter_day, months[month-1], year); //British date format
- */
