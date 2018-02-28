30759a37e7e218d3ee0d8b9a173d345485c40a40
Jean Zonta <wiki.anon@yahoo.com.br>
2018-02-28 12:14:10 -0300

    Sources code C/C++

--- Regex/date/ex2/date.cxx
+++ Regex/date/ex2/date.cxx
@@ -14,7 +14,7 @@ struct date
  {
   std::smatch date_smatch;
   //Regex valida anos bisextos e nao bisextos no formato yyyy/mm/dd ou yyyy/str/dd ou yyyy/Str/dd
-  const std::regex pattern(R"(^(?:\d{4}([/])(?:(?:(?:(?:0?[13578]|1[02]|[Jj](?:an|ul)|[Mm]a[ry]|[Aa]ug|[Oo]ct|[Dd]ec)([/])(?:0?[1-9]|[1-2][0-9]|3[01]))|(?:(?:0?[469]|11|[Aa]pr|[Jj]un|[Ss]ep|[Nn]ov)([/])(?:0?[1-9]|[1-2][0-9]|30))|(?:(0?2|[Ff]eb)([/])(?:0?[1-9]|1[0-9]|2[0-8]))))|(?:(?:\d{2}(?:0[48]|[2468][048]|[13579][26]))|(?:(?:[02468][048])|[13579][26])00)([/])(0?2|[Ff]eb)([/])29)$)"); 
+  const std::regex pattern(R"(^(?:\d{4}([/])(?:(?:(?:0?[13578]|1[02]|[Jj](?:an|ul)|[Mm]a[ry]|[Aa]ug|[Oo]ct|[Dd]ec)([/])(?:0?[1-9]|[1-2][0-9]|3[01]))|(?:(?:0?[469]|11|[Aa]pr|[Jj]un|[Ss]ep|[Nn]ov)([/])(?:0?[1-9]|[1-2][0-9]|30))|(?:(0?2|[Ff]eb)([/])(?:0?[1-9]|1[0-9]|2[0-8])))|(?:(?:\d{2}(?:0[48]|[2468][048]|[13579][26]))|(?:(?:[02468][048])|[13579][26])00)([/])(0?2|[Ff]eb)([/])29)$)"); 
   
   /*//Regex valida anos bisextos e nao bisextos no formato dd/mm/yyyy ou dd/str/yyyy ou dd/Str/yyyy
   const std::regex pattern(
