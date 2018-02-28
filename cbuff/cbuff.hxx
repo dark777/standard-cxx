df0a95d9c006d59175007cd83f523f623f5ac068
Jean Zonta <wiki.anon@yahoo.com.br>
2018-02-28 14:43:32 -0300

    Sources code C/C++

--- cbuff/cbuff.hxx
+++ cbuff/cbuff.hxx
@@ -24,17 +24,29 @@ void cbuff(void);
 
  #include <stdio_ext.h>
  
-#elif defined(_WIN32) || defined(_WIN64) || defined(__WINDOWS__)
+#elif defined(_WIN32) || defined(_WIN64) || defined(__WINDOWS__) || \
+defined(__unix__) || defined(__sun) || defined(__FreeBSD__) || \
+defined(__OpenBSD__) || defined(__APPLE__) || defined(__hpux) || \
+defined(__osf__) || defined(__sgi) || defined(_AIX)
 
  #include <stdio.h>
 
 #endif
 
 
-
-
-
-
+/*
+__linux__       Defined on Linux
+__unix__        Defined on unix
+__sun           Defined on Solaris
+__FreeBSD__     Defined on FreeBSD
+__NetBSD__      Defined on NetBSD
+__OpenBSD__     Defined on OpenBSD
+__APPLE__       Defined on Mac OS X
+__hpux          Defined on HP-UX
+__osf__         Defined on Tru64 UNIX (formerly DEC OSF1)
+__sgi           Defined on Irix
+_AIX            Defined on AIX
+*/
 
 
 
