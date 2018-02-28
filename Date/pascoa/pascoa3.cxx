2e271c7d5cfd42f5ae65ec43ffe16e82d54b37c6
Jean Zonta <wiki.anon@yahoo.com.br>
2018-02-27 23:35:37 -0300

    Sources code C/C++

--- Date/pascoa/pascoa3.cxx
+++ Date/pascoa/pascoa3.cxx
@@ -1,5 +1,24 @@
 #include <iostream>
 
+int DomingoDePascoa(int ano)
+{
+ int a = (ano%19);
+ int b = (ano/100);
+ int c = (ano%100);
+ int d = (b/4);
+ int e = (b%4);
+ int f = ((b+8)/25);
+ int g = ((b-f+1)/3);
+ int h = ((19 * a + b - d - g + 15)%30);
+ int i = (c/4);
+ int j = (c%4);
+ int k = ((32 + 2 * e + 2 * i - h - j)%7);
+ int l = ((a + 11 * h + 22 * k)/451);
+ int mes = ((h + k - 7 * l + 114)/31);
+ int dia = (((h + k - 7 * l + 114)%31)+1);
+ //return new dateTime(ano, mes, dia);
+}
+
 /*Calcular o mes da pascoa*/
 int mesdapascoa(int ano)
 {
