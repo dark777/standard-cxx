0ef70cdb816ee5c6437b1898a009635ea68eb744
Jean Zonta <wiki.anon@yahoo.com.br>
2018-02-27 22:50:10 -0300

    Sources code C/C++

--- Regex/extensions/extensions.cxx
+++ Regex/extensions/extensions.cxx
@@ -14,7 +14,7 @@ struct extensions
  {
   std::smatch extensions_smatch;
   //validas arquivos com as extensoes da regex
-  const std::regex pattern(".*[.](h|hpp|hxx|H|HPP|HXX|c|cpp|cxx|C|CPP|CXX|aspx|php|py|java|rb|d|htm|html|HTM|HTML|mp3|css)");
+  const std::regex pattern(".*[.](h|h(?:h|pp|xx|\\++)|H|H(?:H|PP|XX|\\++)|c|c(?:c|pp|xx|\\++)|C|C(?:C|PP|XX|\\++)|h(?:tm|tml)|H(?:TM|TML)|aspx|php|py|java|rb|d|mp3|css)");
   
   return std::regex_match(_file, extensions_smatch, pattern);
  }
@@ -30,24 +30,41 @@ struct extensions
 
 int main()
 {
-  const std::string files[15] = {
+  const std::string files[32] = {
                                  "file1.c",
-                                 "file2.cpp",
-                                 "file3.cxx",
-                                 "file4.C",
-                                 "file5.CPP",
-                                 "file6.CXX",
-                                 "file7.h",
-                                 "file8.hh",
-                                 "file9.hpp",
-                                 "file10.hxx",
-                                 "file11.H",
-                                 "file12.HH",
-                                 "file13.HPP",
-                                 "file14.HXX",
-                                 "file15.aa"
+                                 "file2.cc",
+                                 "file3.cpp",
+                                 "file4.cxx",
+                                 "file5.c++",
+                                 "file6.C",
+                                 "file7.CC",
+                                 "file8.CPP",
+                                 "file9.CXX",
+                                 "file10.C++",
+                                 "file11.h",
+                                 "file12.hh",
+                                 "file13.hpp",
+                                 "file14.hxx",
+                                 "file15.h++",
+                                 "file16.H",
+                                 "file17.HH",
+                                 "file18.HPP",
+                                 "file19.HXX",
+                                 "file20.H++",
+                                 "file21.htm",
+                                 "file22.HTM",
+                                 "file23.HTML",
+                                 "file24.HTML",
+                                 "file25.aspx",
+                                 "file26.php",
+                                 "file27.py",
+                                 "file28.java",
+                                 "file29.rb",
+                                 "file30.d",
+                                 "file31.mp3",
+                                 "file32.css"
                                 };
-    for(int i=0; i<15; i++)
+    for(int i=0; i<32; i++)
      std::cout << extensions(files[i]).print();
       std::cout << "\n";
    return 0;   
