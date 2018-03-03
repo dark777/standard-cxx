c7942778e5830acfe9f70e7b1fdd4e2add5fca21
Jean Zonta <wiki.anon@yahoo.com.br>
2018-03-03 20:31:20 -0300

    Sources code C/C++

--- Overload/date/ex2/main.cxx
+++ Overload/date/ex2/main.cxx
@@ -5,15 +5,15 @@ int main()
  Date date;
  
  std::cout << "\n\tThe date is Int "
-           << "\n\tThe date Int Format..: "<<date
-           << "\n\tThe day is...........: "<< date.day
-           << "\n\tThe month is.........: "<< date.month
-           << "\n\tThe year is..........: "<< date.year
+           << "\n\tThe date Int Format..: " << date
+           << "\n\tThe day is...........: " << date.day
+           << "\n\tThe month is.........: " << date.month
+           << "\n\tThe year is..........: " << date.year
            << "\n\n\tThe date is Str "
-           << "\n\tThe date String Format..: "<< date.toString()
-           << "\n\tThe day is..............: "<< date.day
-           << "\n\tThe month is............: "<< date.month
-           << "\n\tThe year is.............: "<< date.year
+           << "\n\tThe date String Format..: " << date.toString()
+           << "\n\tThe day is..............: " << date.day
+           << "\n\tThe month is............: " << date.month
+           << "\n\tThe year is.............: " << date.year
            << "\n\n";
   return 0;
 }
