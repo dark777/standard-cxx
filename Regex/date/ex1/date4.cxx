e1083581938508173274f95d94d03f57f59b895c
Jean Zonta <wiki.anon@yahoo.com.br>
2018-03-04 06:52:26 -0300

    Sources code C/C++

--- Regex/date/ex1/date4.cxx
+++ Regex/date/ex1/date4.cxx
@@ -22,15 +22,15 @@
 
 int main()
 {
- constexpr char text[]{"29/feb-2020"};
+ std::string text{"29-feb/2020"};
  
  const std::regex re(
                      R"((?:(?:(0?[1-9]|1\d|2[0-8])([-/.])(0?[1-9]|1[0-2]|j(?:an|u[nl])|ma[ry]|a(?:pr|ug)|sep|oct|nov|dec|feb)|(29|30)([-/.])(0?[13-9]|1[0-2]|j(?:an|u[nl])|ma[ry]|a(?:pr|ug)|sep|oct|nov|dec)|(31)([-/.])(0?[13578]|1[02]|jan|ma[ry]|jul|aug|oct|dec))(?:\2|\5|\8)(0{2,3}[1-9]|0{1,2}[1-9]\d|0?[1-9]\d{2}|[1-9]\d{3})|(29)([-/.])(0?2|feb)\12(\d{1,2}(?:0[48]|[2468][048]|[13579][26])|(?:0?[48]|[13579][26]|[2468][048])00)))",
                      std::regex_constants::icase
                     );
  
- std::cmatch match_date;
-
+ std::smatch match_date;
+ 
  if(!std::regex_match(text, match_date, re))std::cout << "\n\tData inválida!!\n\n";
  else
   {
