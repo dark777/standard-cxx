03b289e13fcef5d9af3785f020871b242eac9349
Jean Zonta <wiki.anon@yahoo.com.br>
2018-03-06 04:06:08 -0300

    Sources code C/C++

--- Regex/email/ex3/example6.cxx
+++ Regex/email/ex3/example6.cxx
@@ -15,7 +15,7 @@ int main()
  std::string email[3] = {
                          "text.example@gmail.com",
                          "text.exa_mple@yahoo.com.br",
-                         "text.example@terra.com.br.net",
+                         "text.example@terra.com.br.net"
                         };
  for(int i=0; i<3;i++)
  std::cout << "\n\tEmail: " << email[i] << " : " << (is_email_valid(email[i]) ?" is Valid\n" : " is inValid\n");
