8ed6e6495bd3752263109d4064f594a2ff8dd8a4
Jean Zonta <wiki.anon@yahoo.com.br>
2018-03-09 18:12:53 -0300

    Sources code C/C++

--- Regex/email/ex1/email.cxx
+++ Regex/email/ex1/email.cxx
@@ -20,7 +20,7 @@ struct email
   //const std::regex pattern("(\\w+)(\\.|_)?(\\w*)@(\\w+)(\\.(\\w+))?(\\.(\\w+))");
   
   //valida somente servidores @{hotmail,terra,yahoo,uol,bol} no dominio .com.br e servidor gmail no dominio .com
-  const std::regex pattern("^([a-z0-9._]+@(?:(?:hotmail|terra|yahoo|uol|bol)[.](?:com[.]br)?)?(?:(?:gmail)[.](?:com)?)?)$");
+  const std::regex pattern("^([a-z0-9._]+@((?:hotmail|terra|yahoo|uol|bol)[.](?:com[.]br))?((?:gmail)[.](?:com))?)$");
    
   return std::regex_match(_email, email_smatch, pattern);
  }
@@ -37,24 +37,25 @@ struct email
 
 int main()
 {
- std::string emails[15] = {
+ std::string emails[16] = {
                            "regex_cpp@net.br",
-                           "regex_cpp@terra.com.br",
-                           "regex_cpp@hotmail.com.br.net",
+                           "reg_ex.cpp@org.nz",
+                           "reg_ex.cpp@net.org.br",   
+                           "regex_cpp@terra.com.br.net",
                            "regex_cpp@hotmail.com",
-                           "regex_cpp@yahoo.com.br",
+                           "regex_cpp@hotmail.com.br",
+                           "regex_cpp@yahoo.com",
+                           "regex_cpp@yahoo.com.br",  
                            "regex_cpp@gmail.com",
                            "reg_ex.cpp@gmail.com.br",    
-                           "regex_cpp@uol.com.br",
                            "regex_cpp@uol.com",
+                           "regex_cpp@uol.com.br",
+                           "regex.cpp@bol.com",   
                            "regex_cpp@bol.com.br",
-                           "regex.cpp@bol.com",
-                           "reg_ex.cpp@org.nz",
-                           "reg_ex.cpp@net.org.br",
                            "regex_email@terra.com",
                            "regex_email@terra.com.br"    
                           };
- for(int i=0; i<15; i++)
+ for(int i=0; i<16; i++)
   std::cout << email(emails[i]).print();
    std::cout << "\n";
  return 0;
