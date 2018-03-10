ebe7b2095766ad6eefcbda786c6c20505a4b068b
Jean Zonta <wiki.anon@yahoo.com.br>
2018-03-10 11:06:49 -0300

    Sources code C/C++

--- Date/validate/daycounter.c
+++ Date/validate/daycounter.c
@@ -1,33 +1,39 @@
 #include <stdio.h>
 //https://www.scriptbrasil.com.br/script/c/daycounter.rar
+
+//conta dias deste janeiro
 int Td(int d,int m,int a)
 {
- int mes[13],i,soma=0;
- mes[0]=0;
+ int soma=0; 
+ int mes[12];
+ 
  mes[1]=mes[3]=mes[5]=mes[7]=mes[8]=mes[10]=mes[12]=31;
- if((a%4)==0) mes[2]=29;
- else mes[2]=28;
+ 
  mes[4]=mes[6]=mes[9]=mes[11]=30;
- for(i=1;i<m;i++)soma+=mes[i];
- soma+=d;
+ 
+ mes[2] = (a%4 == 0) ? 29 : 28;
+ 
+ for(int i=1;i<m;i++)soma+=mes[i];
+ soma+=d-1;
  
  return soma;
 }
 
+
 int main()
 {
- int dia,mes,ano,total;
- printf("\nDig dia: ");
+ int dia,mes,ano;
+ 
+ printf("\n\tDigite o dia (01-31): ");
  scanf("%d",&dia);
  
- printf("\nDig mes: (de 1 a 12) ");
+ printf("\n\tDigite o mes (01-12): ");
  scanf("%d",&mes);
  
- printf("\nDig ano: ");
+ printf("\n\tDigite o ano: ");
  scanf("%d",&ano);
  
- total=Td(dia,mes,ano);
- printf("\n\nTotal=%d\n",total);
+ printf("\n\tForam decorridos %d dias desde 01/01/%d\n\n",Td(dia,mes,ano),ano);
  
  return 0;
 } 
