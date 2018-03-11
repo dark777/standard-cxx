1ee0130113b88d6eb81af23bce0907298f99523c
Jean Zonta <wiki.anon@yahoo.com.br>
2018-03-11 09:57:04 -0300

    Sources code C/C++

--- Date/validate/primeirodia3.c
+++ Date/validate/primeirodia3.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
-const char *wd(int day, int month, int year)
+const char *wd1(int day, int month, int year)
+{
+  /* using C99 compound literals in a single line: notice the splicing */
+  return ((const char *[])                                                \
+          {"Monday", "Tuesday", "Wednesday",                              \
+           "Thursday", "Friday", "Saturday", "Sunday"})[                  \
+          (                                                               \
+           day + ((153 * (month + 12 * ((14 - month) / 12) - 3) + 2) / 5) \
+           + (365 * (year + 4800 - ((14 - month) / 12)))                  \
+           + ((year + 4800 - ((14 - month) / 12)) / 4)                    \
+           - ((year + 4800 - ((14 - month) / 12)) / 100)                  \
+           + ((year + 4800 - ((14 - month) / 12)) / 400) - 32045          \
+          ) % 7];
+}
+
+const char *wd2(int day, int month, int year)
 {
  static const char *weekdays[] =
   {
@@ -27,7 +42,7 @@ const char *wd(int day, int month, int year)
 
 int main(void)
 {
- printf("\n\t%02d/%02d/%4d foi %s\n\n", 10, 03, 2018, wd(10, 03, 2018));
- 
+ printf("\n\t%02d/%02d/%4d foi %s", 10, 03, 2018, wd1(10, 03, 2018));
+ printf("\n\t%02d/%02d/%4d foi %s\n\n", 10, 03, 2018, wd2(10, 03, 2018));
  return 0;
 } 
