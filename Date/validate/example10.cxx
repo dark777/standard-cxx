89d102358a69e4e3a614757fde6fd2f02682792f
Jean Zonta <wiki.anon@yahoo.com.br>
2018-03-12 23:45:47 -0300

    Sources code C/C++

--- Date/validate/example10.cxx
+++ Date/validate/example10.cxx
@@ -1,31 +1,86 @@
 #include <iostream>
 
+constexpr unsigned int str2int(const char* str, int h = 0)
+{
+ // not my code but can't remember where I got it from
+ return !str[h] ? 5381 : (str2int(str, h+1)*33) ^ str[h];
+}
+
 std::string current_date(const std::string& format)
 {
  tm dtm;
- 
- time_t timer = time(NULL);
- 
  char temp[32] = "";
- 
- if(format == "yyyy-mm-dd")
+ time_t timer = time(NULL);
+  
+ switch(str2int(format.c_str()))
   {
-   strftime(temp, sizeof(temp) - 1, "%Y-%m-%d", localtime_r(&timer, &dtm));
-  }
- else 
- if(format == "yy-mm-dd")
-  {
-   strftime(temp, sizeof(temp) - 1, "%y-%m-%d", localtime_r(&timer, &dtm));
-  }
- else 
- if(format == "yyyymmdd")
-  {
-   strftime(temp, sizeof(temp) - 1, "%Y%m%d", localtime_r(&timer, &dtm));
-  }
- else 
- if(format == "yymmdd")
-  {
-   strftime(temp, sizeof(temp) - 1, "%y%m%d", localtime_r(&timer, &dtm));
+   case str2int("yyyy-mm-dd"):
+    strftime(temp, sizeof(temp) - 1, "%Y-%m-%d", localtime_r(&timer, &dtm));
+   break;
+   
+   case str2int("dd-mm-yyyy"):
+    strftime(temp, sizeof(temp) - 1, "%d-%m-%Y", localtime_r(&timer, &dtm));
+   break;
+   
+   case str2int("yy-mm-dd"):
+    strftime(temp, sizeof(temp) - 1, "%y-%m-%d", localtime_r(&timer, &dtm));
+   break;
+   
+   case str2int("dd-mm-yy"):
+    strftime(temp, sizeof(temp) - 1, "%d-%m-%y", localtime_r(&timer, &dtm));
+   break;
+      
+   case str2int("yyyy.mm.dd"):
+    strftime(temp, sizeof(temp) - 1, "%Y.%m.%d", localtime_r(&timer, &dtm));
+   break;
+   
+   case str2int("dd.mm.yyyy"):
+    strftime(temp, sizeof(temp) - 1, "%d.%m.%Y", localtime_r(&timer, &dtm));
+   break;
+   
+   case str2int("yy.mm.dd"):
+    strftime(temp, sizeof(temp) - 1, "%y.%m.%d", localtime_r(&timer, &dtm));
+   break;
+   
+   case str2int("dd.mm.yy"):
+    strftime(temp, sizeof(temp) - 1, "%d.%m.%y", localtime_r(&timer, &dtm));
+   break;   
+   
+   case str2int("yyyy/mm/dd"):
+    strftime(temp, sizeof(temp) - 1, "%Y/%m/%d", localtime_r(&timer, &dtm));
+   break;
+   
+   case str2int("dd/mm/yyyy"):
+    strftime(temp, sizeof(temp) - 1, "%d/%m/%Y", localtime_r(&timer, &dtm));
+   break;
+   
+   case str2int("yy/mm/dd"):
+    strftime(temp, sizeof(temp) - 1, "%y/%m/%d", localtime_r(&timer, &dtm));
+   break;
+   
+   case str2int("dd/mm/yy"):
+    strftime(temp, sizeof(temp) - 1, "%d/%m/%y", localtime_r(&timer, &dtm));
+   break;
+   
+   case str2int("yyyymmdd"):
+    strftime(temp, sizeof(temp) - 1, "%Y%m%d", localtime_r(&timer, &dtm));
+   break;
+   
+   case str2int("ddmmyyyy"):
+    strftime(temp, sizeof(temp) - 1, "%d%m%Y", localtime_r(&timer, &dtm));
+   break;
+   
+   case str2int("yymmdd"):
+    strftime(temp, sizeof(temp) - 1, "%y%m%d", localtime_r(&timer, &dtm));
+   break;
+   
+   case str2int("ddmmyy"):
+    strftime(temp, sizeof(temp) - 1, "%d%m%y", localtime_r(&timer, &dtm));
+   break;
+   
+   default:
+    std::cout << "\n\tInvalid!\n\n";
+   break;
   }
   
  return temp;
@@ -34,41 +89,41 @@ std::string current_date(const std::string& format)
 std::string current_time(const std::string& format)
 {
  tm dtm;
- 
- time_t timer = time(NULL);
- 
  char temp[32] = "";
- 
- if(format == "HH:MM:SS")
+ time_t timer = time(NULL);
+   
+ switch(str2int(format.c_str()))
   {
-   strftime(temp, sizeof(temp) - 1, "%H:%M:%S", localtime_r(&timer, &dtm));
+   case str2int("HH:MM:SS"):
+    strftime(temp, sizeof(temp) - 1, "%H:%M:%S", localtime_r(&timer, &dtm));
+   break;
+   
+   case str2int("HH:MM"):
+    strftime(temp, sizeof(temp) - 1, "%H:%M", localtime_r(&timer, &dtm));
+   break;
+   
+   case str2int("HHMMSS"):
+    strftime(temp, sizeof(temp) - 1, "%H%M%S", localtime_r(&timer, &dtm));
+   break;
+   
+   case str2int("HHMM"):
+    strftime(temp, sizeof(temp) - 1, "%H%M", localtime_r(&timer, &dtm));
+   break;
+   
+   default:
+    std::cout << "\n\tInvalid!\n\n";
+   break;
   }
- else
- if(format == "HH:MM")
-  {
-   strftime (temp, sizeof(temp) - 1, "%H:%M", localtime_r(&timer, &dtm));
-  }
- else
- if(format == "HHMMSS")
-  {
-   strftime(temp, sizeof(temp) - 1, "%H%M%S", localtime_r(&timer, &dtm));
-  }
- else
- if(format == "HHMM")
-  {
-   strftime(temp, sizeof(temp) - 1, "%H%M", localtime_r(&timer, &dtm));
-  }
+  
  return temp;
 }
 
 std::string exp_date(int days, int hours, int minutes, int seconds)
 {
  tm dtm;
- 
- time_t t = time(NULL) + seconds + 60 * (minutes + 60 * (hours + 24 * days));
- 
  char date[256] = "";
- 
+ time_t t = time(NULL) + seconds + 60 * (minutes + 60 * (hours + 24 * days));
+  
  strftime(date, sizeof(date), "%A, %d-%b-%Y %H:%M:%S GMT", gmtime_r(&t, &dtm));
  
  return date;
