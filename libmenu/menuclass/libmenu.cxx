9695b381a9f16682100067050a34ff999b52eb09
Jean Zonta <wiki.anon@yahoo.com.br>
2018-03-13 06:06:50 -0300

    Sources code C/C++

--- libmenu/menuclass/libmenu.cxx
+++ libmenu/menuclass/libmenu.cxx
@@ -4,7 +4,7 @@
 /*!
  *
  * @begin @section terms_of_use Terms of Use
- *
+ * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
