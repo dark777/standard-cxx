f7c2cdab0e55ca1dfcbf0f0c71fead4b663fa95f
Jean Zonta <wiki.anon@yahoo.com.br>
2018-03-17 16:26:40 -0300

    Sources code C/C++

--- mysql_connect/cruds/Qt-is-so-CRUD/gestionecole.sql
+++ mysql_connect/cruds/Qt-is-so-CRUD/gestionecole.sql
@@ -143,18 +143,18 @@ CREATE TABLE IF NOT EXISTS `Echeances` (
 --
 
 INSERT INTO `Echeances` (`id`, `nom`) VALUES
-(1, 'Janvier'),
-(2, 'Février'),
-(3, 'Mars'),
-(4, 'Avril'),
-(5, 'Mai'),
-(6, 'Juin'),
-(7, 'Juillet'),
-(8, 'Aout'),
-(9, 'Septembre'),
-(10, 'Octobre'),
-(11, 'Novembre'),
-(12, 'Décembre');
+(1, 'January'),
+(2,'February'),
+(3,'March'),
+(4,'April'),
+(5,'May'),
+(6,'June'),
+(7,'July'),
+(8,'August'),
+(9,'September'),
+(10,'October'),
+(11,'November'),
+(12,'December');
 
 -- --------------------------------------------------------
 
