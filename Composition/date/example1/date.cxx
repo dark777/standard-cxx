51f500ab9d00e1ebdfdc7f707adfcd9c00a4bc91
Jean Zonta <wiki.anon@yahoo.com.br>
2018-03-22 16:45:22 -0300

    Sources code C/C++

--- Composition/date/example1/date.cxx
+++ Composition/date/example1/date.cxx
@@ -56,13 +56,9 @@ struct Date
    
    const std::string currentDateTime()
    {
-    tm  tstruct;
-    time_t now = time(0);
-        
-    tstruct = *localtime(&now);
     // Visit http://en.cppreference.com/w/cpp/chrono/c/strftime
     // for more information about date/time format
-    strftime(buf, sizeof(buf), "%Y-%m-%d.%X", &tstruct);
+    strftime(buf, sizeof(buf), "%Y-%m-%d.%X", now);
 
     return buf;
    }
