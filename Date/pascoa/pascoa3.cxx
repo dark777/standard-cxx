a624500cd94bac68ca5b8bf28b67a71fa1dca3f0
Jean Zonta <wiki.anon@yahoo.com.br>
2018-03-22 23:35:58 -0300

    Sources code C/C++

--- Date/pascoa/pascoa3.cxx
+++ Date/pascoa/pascoa3.cxx
@@ -1,23 +1,13 @@
 #include <iostream>
 
-
-
-int main()
+void carter(int ano)
 {
  int dia;
  int mes;
- int ano;
  
- do{
-    //O Metodo de carter só permite calcular a data
-    //da pascoa entre os anos de 1900 e 2099
-    std::cout << "\n\tdigite um ano entre 1900 e 2099: ";
-    std::cin >> ano;
-    
-    if(ano < 1900 || ano > 2099)std::cout<<"\n\tano invalido!\n\n";
-      
-   }while(ano < 1900 || ano > 2099);
-  
+ //O Metodo de carter só permite calcular a data
+ //da pascoa entre os anos de 1900 e 2099
+ 
   int a = (255 - (11 * ( ano % 19 )));
   int b = (((a - 21) % 30) + 21);
   
@@ -38,5 +28,37 @@ int main()
    }
    
   printf("\n\tO dia da pascoa é: %02d/%02d/%4d\n\n",dia,mes,ano);
+}
+
+void oudin(int ano)
+{
+ int sec = ( ano / 100 );
+ int a = (ano - (19 * ( ano / 19)));
+ int b = ((sec - 17) / 25);
+ int c = ((sec - ( sec / 4 )) - ((sec - b) / 3) + ((19 * a) + 15));
+ int d = (c - (30 * (c / 30 )));
+ int e = (d - (( d / 28 ) * (1 - (1 / 28)) * (29 / (d + 1)) * ((21 - a) / 11)));
+ int f = ((ano + ( ano / 4 )) + (((e + 2) - sec) + ( sec / 4 )));
+ int g = (f - (7 * (f / 7)));
+ int h = (e - g);
+ int mes = (3 + ((h + 40) / 44));
+ int dia = ((h + 28) - (31 * (mes / 4)));
+
+ printf("\n\tO dia da pascoa é: %02d/%02d/%4d\n\n",dia,mes,ano);
+}
+
+int main()
+{
+ int ano;
+ do{
+    std::cout << "\n\tdigite um ano entre 1900 e 2099: ";
+    std::cin >> ano;
+    
+    if(ano < 1900 || ano > 2099)std::cout<<"\n\tano invalido!\n\n";
+      
+   }while(ano < 1900 || ano > 2099);
+   
+ carter(ano);
+ oudin(ano);
  return 0;
 }
