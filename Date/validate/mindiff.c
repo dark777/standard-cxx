bfc16cbdab2a95e42b0fa64040784f5016a00dc5
Jean Zonta <wiki.anon@yahoo.com.br>
2018-03-23 01:17:38 -0300

    Sources code C/C++

--- Date/validate/mindiff.c
+++ Date/validate/mindiff.c
@@ -8,27 +8,27 @@ int mintotal(int hora, int minuto)
 
 int main()
 {
- int h1, m1, h2, m2, r1, r2, total; //variaveis declaradas
+ int h1, m1, h2, m2; //variaveis declaradas
 
  printf("\n\tDigite a hora:minuto inicial: ");
  scanf("%d:%d", &h1, &m1); //armazendando valores da hora inicial
 
- r1 = mintotal(h1, m1);
+ int r1 = mintotal(h1, m1);
  
  printf("\n\tDigite a hora:minuto final: ");
  scanf("%d:%d", &h2, &m2);
 
- r2 = mintotal(h2, m2);
+ int r2 = mintotal(h2, m2);
 
- total = r2-r1;
+ int total = r2-r1;
 
  if(total < 0)
   {
-   total +=1440; //calculo
+   total+=1440; //calculo
    
-   printf("\n\tR1: %02d\n\tR2: %02d\n\t%d minutos de diferenca!\n\n", r1, r2, total); //impressão do resultado
+   printf("\n\t%d minutos de diferenca!\n\n", total); //impressão do resultado
   }
   else
-  printf("\n\tR1: %02d\n\tR2: %02d\n\t%d minutos de diferenca!\n\n", r1, r2, total); //impressão do resultado
+  printf("\n\t%d minutos de diferenca!\n\n", total); //impressão do resultado
  return 0;
 }
